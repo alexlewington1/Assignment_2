a4c2b2bd7df51788698f72e8275adaf979e8747c
Alex <S311480@uos.ac.uk>
2025-11-10 17:08:22 +0000

    updated landing page and added working links to other pages

--- Ipswich_Retail/urls.py
+++ Ipswich_Retail/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.LandingPage, name="LandingPage"),
-    path("about/", views.AboutPage, name="AboutPage"),
+    path("about/", views.AboutUs, name="AboutUs"),
     path("contactus/", views.ContactUs, name="ContactUs"),
     path("products/", views.ProductPage, name="ProductPage"),
 
