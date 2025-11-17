0b55ad2d9f6f0c7a1226ec3ec23ed205211ca14c
Alex <S311480@uos.ac.uk>
2025-11-17 13:10:10 +0000

    added whitenoise to pull static files through to Render

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,6 +123,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
